c469f04d1357417f3dbc271ada8fd0d9eaff04f2
Xeno333 <149852758+Xeno333@users.noreply.github.com>
2025-06-04 01:38:14 -0500

    Add files via upload

--- init.lua
+++ init.lua
@@ -20,6 +20,8 @@ core.register_entity("horror:the_entity", {
             0.5, 1, 0.5
         },
         stepheight = 2,
+
+        glow = 14,
     },
 
     on_activate = function(self, staticdata, dtime_s)
@@ -73,10 +75,10 @@ core.register_entity("horror:the_entity", {
             local vel = player_pos - pos
             local dist = math.sqrt(vel.x*vel.x + vel.y*vel.y + vel.z*vel.z)
 
-            if dist > 0.5 then
-                vel.x = math.min(math.max(vel.x, -1), 1) * 10
-                vel.z = math.min(math.max(vel.z, -1), 1) * 10
-                vel.y = math.min(math.max(vel.y, -1), 1) * 10
+            if dist > 1 then
+                vel.x = math.min(math.max(vel.x, -1), 1) * 16
+                vel.z = math.min(math.max(vel.z, -1), 1) * 16
+                vel.y = math.min(math.max(vel.y, -1), 1) * 16
 
                 self.object:set_velocity(vel)
             else
@@ -95,6 +97,8 @@ core.register_entity("horror:the_entity", {
                             if math.random(1, 3) ~= 1 then
                                 core.sound_play({name = "the_entity_attack", gain = 2, pitch = 1.5}, {pos = self.object:get_pos(), max_hear_distance = 64}, true)
                                 player:set_hp(0, "slane")
+                            else
+                                core.sound_play({name = "the_entity_growl", gain = 2, pitch = 0.75}, {pos = self.object:get_pos(), max_hear_distance = 64}, true)
                             end
 
                             core.after(0.5, function()
@@ -126,9 +130,11 @@ core.register_entity("horror:the_entity", {
 
 local spooks = {
     {sound = {name = "the_entity_attack", pitch = 1.5}, gain_range = {min = 0.2, max = 1}},
-    {sound = {name = "breaths_1"}, gain_range = {min = 0.2, max = 2}},
-    {sound = {name = "breaths_2"}, gain_range = {min = 0.2, max = 2}},
-    {sound = {name = "breaths_3"}, gain_range = {min = 0.2, max = 2}},
+    {sound = {name = "breaths_1"}, gain_range = {min = 1, max = 3}},
+    {sound = {name = "breaths_2"}, gain_range = {min = 1, max = 3}},
+    {sound = {name = "breaths_3"}, gain_range = {min = 1, max = 3}},
+    {sound = {name = "whistle_1"}, gain_range = {min = 2, max = 4}},
+    {sound = {name = "whistle_2"}, gain_range = {min = 2, max = 4}},
 }
 
 
@@ -139,15 +145,40 @@ function spook()
         local connected = core.get_connected_players()
         for _, player in pairs(connected) do
             local player_pos = player:get_pos()
+            local pos = vector.new(math.random(-16, 16)+player_pos.x, math.random(-4, 16)+player_pos.y, math.random(-16, 16)+player_pos.z)
 
-            if math.random(1, 10) == 1 then
-                core.add_entity(vector.new(math.random(-32, 32)+player_pos.x, math.random(-4, 16)+player_pos.y, math.random(-32, 32)+player_pos.z), "horror:the_entity")
+            local num = math.random(1, 20)
+            if num == 1 then
+                core.add_entity(pos, "horror:the_entity")
+
+            elseif num < 5 then
+                print("A")
+                core.add_particlespawner({
+                    amount = 64,
+                    time = 6,
+                    texture = "particle_1.png",
+                    glow = 14,
+
+                    minpos = pos,
+                    maxpos = pos,
+
+                    minvel = vector.new(-2, -2, -2),
+                    maxvel = vector.new(2, 2, 2),
+
+                    minexptime = 0.1,
+                    maxexptime = 0.5,
+
+                    minsize = 10,
+                    maxsize = 40
+                })
+
+                core.sound_play({name = "the_entity_attack", gain = 2}, {pos = pos, max_hear_distance = 32}, true)
 
             else
                 local spook_selected = spooks[math.random(1, #spooks)]
                 local sound = spook_selected.sound
                 sound.gain = math.random(10 * spook_selected.gain_range.min, 10 * spook_selected.gain_range.max) / 10
-                core.sound_play(sound, {pos = player_pos, max_hear_distance = 16}, true)
+                core.sound_play(sound, {pos = pos, max_hear_distance = 32}, true)
             end
         end
 
