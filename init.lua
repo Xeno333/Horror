5a0ac45b20898ac2c09aca74b31bf154aacde4df
Xeno333 <Xeno333@mailfence.com>
2025-06-04 18:00:55 -0500

    updates

--- init.lua
+++ init.lua
@@ -1,141 +1,25 @@
 horror = {}
 
+local path = core.get_modpath("horror") .. "/"
+
+dofile(path .. "mobs.lua")
 
 
-core.register_entity("horror:the_entity", {
-    initial_properties = {
-        visual = "sprite",
-        textures = {
-            "the_entity.png"
-        },
-        visual_size = {x = 2, y = 4, z = 2},
-
-        pointable = false,
-        physical = true,
-        is_visible = false,
-
-        collide_with_objects = false,
-        collisionbox = {
-            -0.5, -1, -0.5,
-            0.5, 1, 0.5
-        },
-        stepheight = 2,
-
-        glow = 14,
-    },
-
-    on_activate = function(self, staticdata, dtime_s)
-        local pos = self.object:get_pos()
-        local near_objects = core.get_objects_inside_radius(pos, 64)
-
-        local player = nil
-        for _, obj in pairs(near_objects) do
-            if obj:is_valid() and obj:is_player() then
-                player = obj
-                break
-            end
-        end
-        if player == nil then
-            self.object:remove()
-            return
-        end
-
-        self.player = player:get_player_name()
-
-        core.sound_play({name = "the_entity_spawns", gain = 2}, {pos = self.object:get_pos(), max_hear_distance = 64}, true)
-        self.sound = core.sound_play({name = "breaths_1", gain = 2}, {pos = self.object:get_pos(), max_hear_distance = 64, loop = true}, false)
-
-        core.after(4, function()
-            if self.object:is_valid() then
-                local prop = self.object:get_properties()
-                prop.is_visible = true
-                core.after(1, function()
-                    if self.object:is_valid() then
-                        self.spawned = true
-                    end
-                end)
-                self.object:set_properties(prop)
-            end
-        end)
-    end,
-    on_step = function(self, dtime, moveresult)
-        if self.spawned then
-            local player = core.get_player_by_name(self.player)
-            if not player then
-                if self.sound then
-                    core.sound_stop(self.sound)
-                end
-                self.object:remove()
-                return
-            end
-
-            local player_pos = player:get_pos() + player:get_look_dir()
-            local pos = self.object:get_pos()
-
-            local vel = player_pos - pos
-            local dist = math.sqrt(vel.x*vel.x + vel.y*vel.y + vel.z*vel.z)
-
-            if dist > 0.5 and not self.attacking then
-                local ratio = math.abs(math.max(vel.x, vel.y, vel.z))
-                vel.x = (vel.x / ratio) * 16
-                vel.y = (vel.y / ratio) * 16
-                vel.z = (vel.z / ratio) * 16
-
-                self.object:set_velocity(vel)
-            else
-                self.attacking = true
-                
-                local player_lighting = player:get_lighting()
-                local exposure = player_lighting.exposure.exposure_correction
-                player_lighting.exposure.exposure_correction = -4
-                player:set_lighting(player_lighting)
-
-                self.object:set_velocity(vector.new(0, 0, 0))
-
-                core.after(0.5, function()
-                    if self.object:is_valid() then
-                        if player:is_valid() then
-                            if math.random(1, 3) ~= 1 then
-                                core.sound_play({name = "the_entity_attack", gain = 2, pitch = 1.5}, {pos = self.object:get_pos(), max_hear_distance = 64}, true)
-                                player:set_hp(0, "slane")
-                            else
-                                core.sound_play({name = "the_entity_growl", gain = 2, pitch = 0.75}, {pos = self.object:get_pos(), max_hear_distance = 64}, true)
-                            end
-
-                            core.after(0.5, function()
-                                if player:is_valid() then
-                                    local player_lighting = player:get_lighting()
-                                    player_lighting.exposure.exposure_correction = exposure
-                                    player:set_lighting(player_lighting)
-                                end
-                            end)
-                        end
-
-                        if self.sound then
-                            core.sound_stop(self.sound)
-                        end
-                        self.object:remove()
-                    end
-                end)
-            end
-        end
-    end,
-    on_deactivate = function(self, removal)
-        if self.sound then
-            core.sound_stop(self.sound)
-        end
-    end,
-    on_punch = function(self, puncher, time_from_last_punch, tool_capabilities, dir, damage)
-    end,
-})
 
 local spooks = {
-    {sound = {name = "the_entity_attack", pitch = 1.5}, gain_range = {min = 0.2, max = 1}},
-    {sound = {name = "breaths_1"}, gain_range = {min = 1, max = 3}},
-    {sound = {name = "breaths_2"}, gain_range = {min = 1, max = 3}},
-    {sound = {name = "breaths_3"}, gain_range = {min = 1, max = 3}},
-    {sound = {name = "whistle_1"}, gain_range = {min = 2, max = 4}},
-    {sound = {name = "whistle_2"}, gain_range = {min = 2, max = 4}},
+    {sound = {name = "horror_the_entity_attack", pitch = 1.5}, gain_range = {min = 0.2, max = 1}},
+    {sound = {name = "horror_breaths_1"}, gain_range = {min = 1, max = 3}},
+    {sound = {name = "horror_breaths_2"}, gain_range = {min = 1, max = 3}},
+    {sound = {name = "horror_breaths_3"}, gain_range = {min = 1, max = 3}},
+    {sound = {name = "horror_whistle_1", pitch = 1.5}, gain_range = {min = 2, max = 4}},
+    {sound = {name = "horror_whistle_2", pitch = 1.5}, gain_range = {min = 2, max = 4}},
+    {sound = {name = "horror_whistle_2", pitch = 2}, gain_range = {min = 2, max = 4}},
+}
+
+local spook_mobs = {
+    "horror:the_entity",
+    "horror:chaser",
+    "horror:reaper"
 }
 
 
@@ -151,13 +35,13 @@ function spook()
             local num = math.random(1, 20)
 
             if num < 3 then
-                core.add_entity(pos, "horror:the_entity")
+                core.add_entity(pos, spook_mobs[math.random(1, #spook_mobs)])
 
             elseif num <= 5 then
                 core.add_particlespawner({
                     amount = 64,
                     time = 6,
-                    texture = "particle_1.png",
+                    texture = "horror_particle_1.png",
                     glow = 14,
 
                     minpos = pos,
@@ -173,16 +57,16 @@ function spook()
                     maxsize = 40
                 })
 
-                core.sound_play({name = "the_entity_attack", gain = 2}, {pos = pos, max_hear_distance = 32}, true)
+                core.sound_play({name = "horror_the_entity_attack", gain = 2}, {pos = pos, max_hear_distance = 32}, true)
 
             elseif num <= 8 then
                 local id = player:hud_add({
                     type = "image",
-                    text = "particle_1.png",
+                    text = "horror_particle_1.png",
                     scale = {x = 256, y = 256},
                     alignment = {x = 0.5, y = 0.4}
                 })
-                core.sound_play({name = "the_entity_attack", gain = 2}, {pos = player_pos, max_hear_distance = 32}, true)
+                core.sound_play({name = "horror_the_entity_attack", gain = 2}, {pos = player_pos, max_hear_distance = 32}, true)
                 core.after(0.1, function()
                     if player:is_valid() then
                         player:hud_remove(id)
---
+++ mobs.lua
@@ -0,0 +1,365 @@
+
+
+
+core.register_entity("horror:lightning", {
+    initial_properties = {
+        visual = "upright_sprite",
+        textures = {
+            "horror_lightning.png",
+            "horror_lightning.png"
+        },
+        visual_size = {x = 2, y = 64, z = 2},
+
+        pointable = false,
+        physical = false,
+        is_visible = true,
+
+
+        glow = 14,
+    },
+
+    on_activate = function(self, staticdata, dtime_s)
+        self.object:set_rotation(vector.new(math.random(0, 50) / 100, math.random(0, 314) / 100, 0))
+    
+        core.sound_play({name = "horror_lightning", gain = 4, pitch = 0.2}, {pos = self.object:get_pos(), max_hear_distance = 64}, true)
+
+        core.after(0.5, function()
+            core.sound_play({name = "horror_thunder", gain = 4, pitch = 0.5}, {pos = self.object:get_pos(), max_hear_distance = 64}, true)
+            if self.object:is_valid() then
+                self.object:remove()
+            end
+        end)
+    end,
+    on_deactivate = function(self, removal)
+        if not removal then
+            self.object:remove()
+        end
+    end,
+})
+
+
+
+
+core.register_entity("horror:the_entity", {
+    initial_properties = {
+        visual = "sprite",
+        textures = {
+            "horror_the_entity.png"
+        },
+        visual_size = {x = 2, y = 4, z = 2},
+
+        pointable = false,
+        physical = true,
+        is_visible = false,
+
+        collide_with_objects = false,
+        collisionbox = {
+            -0.5, -1, -0.5,
+            0.5, 1, 0.5
+        },
+        stepheight = 2,
+
+        glow = 14,
+    },
+
+    on_activate = function(self, staticdata, dtime_s)
+        local pos = self.object:get_pos()
+        local near_objects = core.get_objects_inside_radius(pos, 64)
+
+        local player = nil
+        for _, obj in pairs(near_objects) do
+            if obj:is_valid() and obj:is_player() then
+                player = obj
+                break
+            end
+        end
+        if player == nil then
+            self.object:remove()
+            return
+        end
+
+        self.player = player:get_player_name()
+
+        core.sound_play({name = "horror_the_entity_spawns", gain = 2}, {pos = self.object:get_pos(), max_hear_distance = 64}, true)
+        self.sound = core.sound_play({name = "horror_breaths_1", gain = 2}, {pos = self.object:get_pos(), max_hear_distance = 64, loop = true}, false)
+
+        core.after(4, function()
+            if self.object:is_valid() then
+                local prop = self.object:get_properties()
+                prop.is_visible = true
+                core.after(1, function()
+                    if self.object:is_valid() then
+                        self.spawned = true
+                    end
+                end)
+                self.object:set_properties(prop)
+            end
+        end)
+    end,
+    on_step = function(self, dtime, moveresult)
+        if self.spawned then
+            local player = core.get_player_by_name(self.player)
+            if not player then
+                self.object:remove()
+                return
+            end
+
+            local player_pos = player:get_pos() + player:get_look_dir()
+            local pos = self.object:get_pos()
+
+            local vel = player_pos - pos
+            local dist = math.sqrt(vel.x*vel.x + vel.y*vel.y + vel.z*vel.z)
+
+            if dist > 1 and not self.attacking then
+                local ratio = math.abs(math.max(vel.x, vel.y, vel.z))
+
+                vel.x = (vel.x / ratio) * 10
+                vel.y = (vel.y / ratio) * 10
+                vel.z = (vel.z / ratio) * 10
+
+                self.object:set_velocity(vel)
+            else
+                self.attacking = true
+                
+                local player_lighting = player:get_lighting()
+                local exposure = player_lighting.exposure.exposure_correction
+                player_lighting.exposure.exposure_correction = -4
+                player:set_lighting(player_lighting)
+
+                self.object:set_velocity(vector.new(0, 0, 0))
+
+                core.after(0.5, function()
+                    if self.object:is_valid() then
+                        if player:is_valid() then
+                            if math.random(1, 3) ~= 1 then
+                                core.sound_play({name = "horror_the_entity_attack", gain = 2, pitch = 1.5}, {pos = self.object:get_pos(), max_hear_distance = 64}, true)
+                                player:set_hp(0, "slane")
+                            else
+                                core.sound_play({name = "horror_the_entity_growl", gain = 2, pitch = 0.75}, {pos = self.object:get_pos(), max_hear_distance = 64}, true)
+                            end
+
+                            core.after(0.5, function()
+                                if player:is_valid() then
+                                    local player_lighting = player:get_lighting()
+                                    player_lighting.exposure.exposure_correction = exposure
+                                    player:set_lighting(player_lighting)
+                                end
+                            end)
+                        end
+
+                        self.object:remove()
+                    end
+                end)
+            end
+        end
+    end,
+    on_deactivate = function(self, removal)
+        if self.sound then
+            core.sound_stop(self.sound)
+        end
+        if not removal then
+            self.object:remove()
+        end
+    end,
+})
+
+
+core.register_entity("horror:chaser", {
+    initial_properties = {
+        visual = "sprite",
+        textures = {
+            "horror_chaser.png"
+        },
+        visual_size = {x = 2, y = 4, z = 2},
+
+        pointable = false,
+        physical = true,
+        is_visible = true,
+
+        collide_with_objects = false,
+        collisionbox = {
+            -0.5, -1, -0.5,
+            0.5, 1, 0.5
+        },
+        stepheight = 2,
+        makes_footstep_sound = true,
+
+        glow = 14,
+    },
+
+    on_activate = function(self, staticdata, dtime_s)
+        local pos = self.object:get_pos()
+        local near_objects = core.get_objects_inside_radius(pos, 64)
+
+        local player = nil
+        for _, obj in pairs(near_objects) do
+            if obj:is_valid() and obj:is_player() then
+                player = obj
+                break
+            end
+        end
+        if player == nil then
+            self.object:remove()
+            return
+        end
+
+        self.player = player:get_player_name()
+
+        core.after(4 * math.random(1, 4), function()
+            if self.object:is_valid() then
+                core.sound_play({name = "horror_the_entity_attack", gain = 2, pitch = 0.5}, {pos = self.object:get_pos(), max_hear_distance = 64}, true)
+                core.after(0.25, function()
+                    if self.object:is_valid() then
+                        self.object:set_pos(player:get_pos() + player:get_look_dir() + vector.new(0, 1, 0))
+                    end
+                end)
+                core.after(1, function()
+                    if self.object:is_valid() then
+                        self.object:remove()
+                    end
+                end)
+            end
+        end)
+    end,
+    on_step = function(self, dtime, moveresult)
+        local player = core.get_player_by_name(self.player)
+        if not player then
+            self.object:remove()
+            return
+        end
+
+        if not self.whistled and math.random(1, 2) == 1 then
+            core.sound_play({name = "horror_whistle_2", gain = 4, pitch = 2}, {pos = self.object:get_pos(), max_hear_distance = 64}, true)
+            self.whistled = true
+        end
+
+        local player_pos = player:get_pos() + player:get_look_dir()
+        local pos = self.object:get_pos()
+
+        local vel = player_pos - pos
+        local dist = math.sqrt(vel.x*vel.x + vel.y*vel.y + vel.z*vel.z)
+
+        if dist > 10 then
+            local ratio = math.abs(math.max(vel.x, vel.y, vel.z))
+            vel.x = (vel.x / ratio) * 4
+            vel.y = -9
+            vel.z = (vel.z / ratio) * 4
+
+            self.object:set_velocity(vel)
+        else
+            self.object:set_velocity(vector.new(0, -9, 0))
+        end
+    end,
+    on_deactivate = function(self, removal)
+        if not removal then
+            self.object:remove()
+        end
+    end,
+})
+
+
+
+
+core.register_entity("horror:reaper", {
+    initial_properties = {
+        visual = "sprite",
+        textures = {
+            "horror_reaper.png"
+        },
+        visual_size = {x = 2, y = 4, z = 2},
+
+        pointable = false,
+        physical = false,
+        is_visible = true,
+
+        collide_with_objects = false,
+        collisionbox = {
+            -0.5, -1, -0.5,
+            0.5, 1, 0.5
+        },
+
+        glow = 14,
+    },
+
+    on_activate = function(self, staticdata, dtime_s)
+        local pos = self.object:get_pos()
+        local near_objects = core.get_objects_inside_radius(pos, 64)
+
+        local player = nil
+        for _, obj in pairs(near_objects) do
+            if obj:is_valid() and obj:is_player() then
+                player = obj
+                break
+            end
+        end
+        if player == nil then
+            self.object:remove()
+            return
+        end
+
+        self.player = player:get_player_name()
+        if math.random(1, 2) == 1 then
+            core.sound_play({name = "horror_chains_1", gain = 8, pitch = 0.5}, {pos = player:get_pos(), max_hear_distance = 16}, true)
+        end
+
+        core.after(16, function()
+            if self.object:is_valid() then
+                if math.random(1, 5) ~= 1 then
+                    self.object:remove()
+
+                else
+                    core.sound_play({name = "horror_the_entity_attack", gain = 2, pitch = 0.5}, {pos = self.object:get_pos(), max_hear_distance = 64}, true)
+
+                    core.after(0.25, function()
+                        if self.object:is_valid() then
+                            self.object:set_pos(player:get_pos() + player:get_look_dir() + vector.new(0, 1, 0))
+                        end
+                    end)
+                    core.after(1, function()
+                        if self.object:is_valid() then
+                            self.object:remove()
+
+                            if player:is_valid() then
+                                player:set_hp(0, "reaped")
+                            end
+                        end
+                    end)
+                end
+            end
+        end)
+    end,
+    on_step = function(self, dtime, moveresult)
+        local player = core.get_player_by_name(self.player)
+        if not player then
+            self.object:remove()
+            return
+        end
+
+        local player_pos = player:get_pos() + player:get_look_dir()
+        local pos = self.object:get_pos()
+
+        local vel = player_pos - pos
+        local dist = math.sqrt(vel.x*vel.x + vel.y*vel.y + vel.z*vel.z)
+
+        if dist > 16 then
+            local ratio = math.abs(math.max(vel.x, vel.y, vel.z))
+            vel.x = (vel.x / ratio) * 4
+            vel.y = (vel.y / ratio) * 4
+            vel.z = (vel.z / ratio) * 4
+
+            self.object:set_velocity(vel)
+        else
+            self.object:set_velocity(vector.new(0, 0, 0))
+        end
+    end,
+    on_deactivate = function(self, removal)
+        local pos = self.object:get_pos()
+        core.add_entity(pos + vector.new(1, 0, 0), "horror:lightning")
+        core.add_entity(pos, "horror:lightning")
+        core.add_entity(pos + vector.new(0, 0, 1), "horror:lightning")
+
+        if not removal then
+            self.object:remove()
+        end
+    end,
+})
